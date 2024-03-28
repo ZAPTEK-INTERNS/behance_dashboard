d9ab38a06449cf9846736f8b022dd1b65057807e
Bismark <bismarkb609@gmail.com>
2024-03-28 21:19:42 +0000

    updated starter folders with mantine ui

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import '@mantine/core/styles.css';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "A simple dashboard application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={inter.className}>
+       <MantineProvider>
+        {children}
+       </MantineProvider>
+      </body>
+    </html>
+  );
+}
